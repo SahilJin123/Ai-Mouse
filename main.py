8d5fcbdd7683c6517a143a2c37b5b92e7efb3dbd
Sahil Jindal <107670763+SahilJin123@users.noreply.github.com>
2023-01-03 03:34:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+cap = cv2.VideoCapture(0)
+hand_detector = mp.solutions.hands.Hands()    #yeh hand ko detect karega
+drawing_utils = mp.solutions.drawing_utils
+screen_width,screen_height = pyautogui.size()
+index_y =0
+while True:
+    _,frame = cap.read()
+    frame = cv2.flip(frame,1)
+    frame_height,frame_width,_ = frame.shape
+    rgb_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    output = hand_detector.process(rgb_frame)  #yeh detected hands ko process karega aur uske related saari info dega
+    hands = output.multi_hand_landmarks  #yeh outputed hand ke landmarks ke coordinates dega
+    if hands:
+        for hand in hands:
+            drawing_utils.draw_landmarks(frame,hand)  #yeh hand ke evry point ko mark karega
+            landmarks = hand.landmark
+            for id , landmark in enumerate(landmarks):
+                x = int(landmark.x *frame_width)
+                y = int(landmark.y *frame_height)
+                print(x,y)
+                if id == 8:
+                  cv2.circle(img = frame,center = (x,y),radius = 10 , color = (0,255,255))
+                  index_x = screen_width/frame_width*x
+                  index_y = screen_height/frame_height*y
+                  pyautogui.moveTo(index_x,index_y)
+                if id == 12:
+                  cv2.circle(img = frame,center = (x,y),radius = 10 , color = (0,255,255))
+                  thumb_x = screen_width/frame_width*x
+                  thumb_y = screen_height/frame_height*y
+                  print('Outside',abs(index_y - thumb_y))
+                  if abs(index_y - thumb_y) < 50:
+                      pyautogui.click()
+                      pyautogui.sleep(1)
+                      print("Click****************************************************************")
+
+    cv2.imshow("Virtual Mouse",frame)
+    cv2.waitKey(1)
